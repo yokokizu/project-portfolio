2af8a8a4f99f0ae4028b592ecedafc949a7d16f3
yokokizu <yoko.kizu@kaplan.se>
2021-10-20 14:38:41 +0200

    About, Skills

---
+++ script.js
@@ -0,0 +1,4 @@
+// Lucky Color Game
+const colors = ['Red', 'Blue', 'Yellow', 'Pink', 'White', 'Block', 'Green', 'Brown']
+const showGameBtn = document.querySelector('.games h3');
+console.log(showGameBtn);
