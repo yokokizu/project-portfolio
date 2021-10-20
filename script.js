6fc0de48484ecb48330948af5c90873b8a08c21e
yokokizu <yoko.kizu@kaplan.se>
2021-10-20 15:27:05 +0200

    first JS

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// Show Games
+const showGameBtn = document.querySelectorAll('.games h3');
+for (let i = 0; i < showGameBtn.length; i++){
+    showGameBtn[i].addEventListener('click', event => {
+        let showStage = showGameBtn[i].nextElementSibling;
+        let stageStatus = window.getComputedStyle(showStage).display;
+        if(stageStatus === 'none'){
+            showStage.style.display = 'block';
+        } else {
+            showStage.style.display = 'none';
+        }
+    })
+}
+
+
+
 // Lucky Color Game
 const colors = ['Red', 'Blue', 'Yellow', 'Pink', 'White', 'Block', 'Green', 'Brown']
-const showGameBtn = document.querySelector('.games h3');
-console.log(showGameBtn);
+
