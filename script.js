510b65385427dbd01a093c52fafb19e70145cef1
yokokizu <yoko.kizu@kaplan.se>
2021-10-21 13:01:46 +0200

    JS

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+// Show next element
+const showNextOne = arr => {
+    for (let i = 0; i < showGameBtn.length; i++){
+        arr[i].addEventListener('click', event => {
+            let showStage = arr[i].nextElementSibling;
+            let stageStatus = window.getComputedStyle(showStage).display;
+            if(stageStatus === 'none'){
+                showStage.style.display = 'block';
+            } else {
+                showStage.style.display = 'none';
+            }
+        })
+    }
+}
 // Show Games
 const showGameBtn = document.querySelectorAll('.games h3');
-for (let i = 0; i < showGameBtn.length; i++){
-    showGameBtn[i].addEventListener('click', event => {
-        let showStage = showGameBtn[i].nextElementSibling;
-        let stageStatus = window.getComputedStyle(showStage).display;
-        if(stageStatus === 'none'){
-            showStage.style.display = 'block';
-        } else {
-            showStage.style.display = 'none';
-        }
-    })
-}
-
-//ffff
+showNextOne(showGameBtn);
 
 // Lucky Color Game
-const colors = ['Red', 'Blue', 'Yellow', 'Pink', 'White', 'Block', 'Green', 'Brown']
+const colors = ['red', 'blue', 'yellow', 'pink', 'white', 'green', 'black', 'green', 'brown', 'purple', 'violet', 'gray'];
+const theLuckyColor = document.getElementById('theLuckyColor');
+const colorList = document.getElementById('colorList');
+const playAgain = document.getElementById('playAgain');
+colorList.innerHTML = colors.join(', ');
 
+const  chooseColor = (colorArray, colorPlace) => {
+    colorNum = Math.floor(Math.random() * (colorArray.length - 1));
+    colorPlace.color = colorArray[colorNum];
+    colorPlace.innerHTML = colorPlace.color;
+    colorPlace.style.backgroundColor = colorPlace.color;
+    const lightColor = ['yellow', 'white', 'pink'];
+    if(lightColor.includes(colorPlace.color)){
+        colorPlace.style.color = 'black';
+    } else {
+        colorPlace.style.color = 'white';
+    }
+} 
+chooseColor(colors, theLuckyColor);
+playAgain.addEventListener('click', event => {chooseColor(colors, theLuckyColor)});
